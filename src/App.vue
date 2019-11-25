332fb8ecb45ec0fa3bac3ee4ce3b8a184b6f0f1b
Kubra <kubra.yavuz4@ogr.sakarya.edu.tr>
2019-11-25 11:57:18 +0300

    Navbar used in VueJs

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <nav class="navbar navbar-inverse">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <a class="navbar-brand" href="#appHome">WebSite</a>
+        </div>
+        <ul class="nav navbar-nav">
+          <li><a href="#appHome" @click="selectedComponent = 'appHome'">Home</a></li>
+          <li><a href="#appAbout" @click="selectedComponent = 'appAbout'">About</a></li>
+          <li><a href="#appContact" @click="selectedComponent = 'appContact'">Contact</a></li>
+        </ul>
+      </div>
+    </nav>
+    <component :is="selectedComponent"></component>
+  </div>
+</template>
+
+<script>
+  import Home from "./components/Home";
+  import About from "./components/About";
+  import Contact from "./components/Contact";
+  export default {
+    data:function(){
+      return{
+        selectedComponent : ""
+      }
+    },
+    components:{
+      appHome : Home,
+      appAbout : About,
+      appContact : Contact
+    }
+  }
+</script>
+<style></style>
